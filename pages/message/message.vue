1b225fd30f36e5ffb108966af714b555ba2bce2e
柳斐 <1938358863@qq.com>
2021-10-04 13:54:53 +0800

    my first uni project

---
+++ pages/message/message.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		<p>消息页</p>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/message/personal.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		<h1>个人主页</h1>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
